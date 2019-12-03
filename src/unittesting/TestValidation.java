3cb18301741756c77d2902eff3186e8d97a969b3
vinitpardhi <vinitpardhi@gmail.com>
2019-12-03 09:04:49 +0530

    Added a new test case for validating name

--- src/unittesting/TestValidation.java
+++ src/unittesting/TestValidation.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static utils.ValidationRules.*;
 
 public class TestValidation {
 
@@ -18,7 +19,13 @@ public class TestValidation {
 
 	@Test
 	public void testValidateName() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
+		try {
+			assertEquals("Ashish", validateName("Ashish"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
