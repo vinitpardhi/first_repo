1abb0f5e55859b73da74843a16d289a2f435c2b6
vinitpardhi <vinitpardhi@gmail.com>
2019-12-03 08:53:20 +0530

    New JUnit test file

---
+++ src/unittesting/TestValidation.java
@@ -0,0 +1,24 @@
+package unittesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestValidation {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testValidateName() {
+		fail("Not yet implemented");
+	}
+
+}
